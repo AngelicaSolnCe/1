4f3a00f1a2ec1126bdeee466f0a8aeb5a09b5677
AngelicaSolnCe <76481401+AngelicaSolnCe@users.noreply.github.com>
2021-06-05 00:08:47 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ОХ_ОХ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            {
+                Console.WriteLine("Введите высоту ребра куба: ");
+                double h = Convert.ToDouble(Console.ReadLine());
+                Console.Write("\n");
+                Console.Write("Высота ребра куба = {0}\n", h);
+                double s = 6 * (h * h);
+                double v = h * h * h;
+                Console.Write("Площадь куба = {0} , объем куба = {1}", s, v);
+                Console.ReadLine();
+            }
+        }
+    }
+}
