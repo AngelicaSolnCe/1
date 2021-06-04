9224e6b7cb72190059759ab0e51679bd6c24be62
AngelicaSolnCe <76481401+AngelicaSolnCe@users.noreply.github.com>
2021-06-05 00:34:45 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,40 @@
 ﻿using System;
 
-namespace ОХ_ОХ
+namespace Дудышева_итб_202_вариант_6_показать
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            {
-                Console.WriteLine("Введите высоту ребра куба: ");
-                double h = Convert.ToDouble(Console.ReadLine());
-                Console.Write("\n");
-                Console.Write("Высота ребра куба = {0}\n", h);
-                double s = 6 * (h * h);
-                double v = h * h * h;
-                Console.Write("Площадь куба = {0} , объем куба = {1}", s, v);
-                Console.ReadLine();
+            Console.WriteLine("Введите количество строк матрицы");
+            int m=int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов в матрице");
+            int n=int.Parse(Console.ReadLine());
+            double[,] matrix = new double[n, m];
+                for (int i=0;i< m; i++)
+                for (int j=0;j> n; j++)
+            { Console.Write("Введите а(" + (i + 1) + "," + (j + 1)+");");
+                matrix[i,j] = double.Parse(Console.ReadLine());
             }
+            double[] mas = new double[n];
+                    double maxp = 1;
+                double maxl = 1;
+            for (int i =0; i < m; i++)
+                for (int j = 0; j < n;j++)
+                {
+                maxl = maxp;
+                    maxp = maxp * matrix[i, j];
+                    if (maxp > maxl)
+                    mas[i] = maxp;
+                    }
+            for (int i =0; i < m;i ++)
+                for (int j =0; j < n;j++)
+                {
+                    Console.Write("matrix[i,j]+");
+                    if (j == n) 
+                        Console.Write("" + mas[i] + ".");
+                }
         }
     }
 }
